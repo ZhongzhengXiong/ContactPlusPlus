17e91f359fcb94a1d03dd9fbb008597d7463dca0
zzxiong <tomcatxiong2@gmail.com>
2018-10-26 11:10:42 +0800

    render.js try

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {BrowserWindow} = require('electron').remote
+const path = require('path')
+
+const newWindowBtn = document.getElementById('new-window')
+
+newWindowBtn.addEventListener('click', event=>{
+    const modalPath = path.join('modal.html')
+    let win = new BrowserWindow({width: 400, height: 320})
+
+    win.on('close', ()=>{win = null})
+    win.loadURL(modalPath)
+    win.show()
+})
